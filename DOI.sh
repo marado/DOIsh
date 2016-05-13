c6a891c16f254b49c7a5429619072294921440d0
Marcos Marado <mindboosternoori@gmail.com>
2016-05-14 00:24:09 +0100

    Initial release

---
+++ DOI.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]
+then 
+  echo "Usage:"
+  echo "  DOI.sh 10.1210/en.2005-0640" 
+  echo "  DOI.sh 10.1210/en.2005-0640 --csv" 
+  echo "  DOI.sh --csv 10.1210/en.2005-0640" 
+  exit
+fi
+
+if [ $# -eq 2 ]
+then
+  if [ "$1" = "--csv" ]
+  then
+    DOI=$2
+  elif [ "$2" = "--csv" ]
+  then
+    DOI=$1
+  else
+    echo "Usage:"
+    echo "  DOI.sh 10.1210/en.2005-0640" 
+    echo "  DOI.sh 10.1210/en.2005-0640 --csv" 
+    echo "  DOI.sh --csv 10.1210/en.2005-0640" 
+    exit
+  fi
+else
+  DOI=$1
+fi
+
+echo "Searching for DOI $DOI:";
+echo ""
+
+result=$(wget "http://api.crossref.org/works/$DOI" -o /dev/null -O - | jsawk \
+  '
+  result = "";
+  for (var i in this.message) { 
+    switch (i) {
+      case "indexed":
+      case "reference-count":
+      case "published-print":
+      case "published-online":
+      case "source":
+      case "prefix":
+      case "member":
+        break;
+      case "created":
+        // believe it or not, jsshell is buggy. doing this the ugly way:
+        for (var j in this.message.created) {
+          if (j == "date-time") result += "created: " + this.message.created[j] + "\n";
+        }
+        break;
+      case "deposited":
+        // believe it or not, jsshell is buggy. doing this the ugly way:
+        for (var j in this.message.deposited) {
+          if (j == "date-time") result += "deposited: " + this.message.deposited[j] + "\n";
+        }
+        break;
+      case "issued":
+        // believe it or not, jsshell is buggy. doing this the ugly way:
+        for (var j in this.message.issued) {
+          if (j == "date-parts") result += "issued: " + this.message.issued[j] + "\n";
+        }
+        break;
+      case "author":
+        if (this.message.author.length == 0) break;
+        result += "authors: ";
+        for (var j = 0; j < this.message.author.length; j++) { 
+          result += this.message.author[j].given + " " +this.message.author[j].family;
+          if (j + 1 < this.message.author.length) result += "; ";
+        }
+        result += "\n";
+        break;
+      default:
+        result += i +": " + this.message[i] + "\n";
+    }
+  } 
+  return result;
+  ')
+
+if [ $# -eq 2 ]
+then
+  # TODO: --csv
+  echo "CSV output not implemented yet. Showing regular output:"
+  echo ""
+  echo "$result";
+else
+  echo "$result";
+fi
---
+++ README.md
@@ -0,0 +1,7 @@
+DOIsh
+=====
+
+DOIsh (reads "DOI shell") is a shell script intended to give you the most
+possible information about an article, given its DOI.
+
+Usage: `DOI.sh 10.1210/en.2005-0640`
